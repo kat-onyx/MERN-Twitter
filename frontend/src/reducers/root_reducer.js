d193ebc30a89aa9edc96154e5d295e5c13293f4a
kat <sarah.kathryn.peters@gmail.com>
2018-12-28 15:03:36 -0800

    Completing components

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import session from './errors_reducer'
+import sessionErrors from './errors_reducer'
 
 const RootReducer = combineReducers({
-    session
+    session,
+    sessionErrors
 });
 
 export default RootReducer;
